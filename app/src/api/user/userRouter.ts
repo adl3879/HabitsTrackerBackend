f82cbea18731bdb5910d5fa693462a7121aa1dc8
Toyosi Adekanmbi <57632354+adl3879@users.noreply.github.com>
2024-05-29 14:00:23 +0000

    re

--- app/src/api/user/userRouter.ts
+++ app/src/api/user/userRouter.ts
@@ -1,8 +1,5 @@
 import { Hono } from 'hono';
 
-import { InsertUser, insertUserSchema } from '@/common/db/schema/user';
-import { validator } from '@/common/utils/validator';
-
 import { userService } from './userService';
 
 const user = new Hono();
@@ -12,22 +9,22 @@ user.get('/', async (c) => {
   return c.json(serviceResponse, serviceResponse.statusCode);
 });
 
-user.post('/register', validator(insertUserSchema), async (c) => {
-  const { body } = c.req.valid('form');
-  const user = {
-    name: body.name as string,
-    email: body.email as string,
-    password: body.password as string,
-    role: 'user',
-  } satisfies InsertUser;
-  const serviceResponse = await userService.registerUser(user);
-  return c.json(serviceResponse, serviceResponse.statusCode);
-});
+// user.post('/register', validator(insertUserSchema), async (c) => {
+//   const { body } = c.req.valid('form');
+//   const user = {
+//     name: body.name as string,
+//     email: body.email as string,
+//     password: body.password as string,
+//     role: 'user',
+//   } satisfies InsertUser;
+//   const serviceResponse = await userService.registerUser(user);
+//   return c.json(serviceResponse, serviceResponse.statusCode);
+// });
 
-user.post('/login', validator(insertUserSchema.pick({ email: true, password: true })), async (c) => {
-  const body = await c.req.parseBody();
-  const serviceResponse = await userService.loginUser(body.email as string, body.password as string);
-  return c.json(serviceResponse, serviceResponse.statusCode);
-});
+// user.post('/login', validator(insertUserSchema.pick({ email: true, password: true })), async (c) => {
+//   const body = await c.req.parseBody();
+//   const serviceResponse = await userService.loginUser(body.email as string, body.password as string);
+//   return c.json(serviceResponse, serviceResponse.statusCode);
+// });
 
 export default user;
